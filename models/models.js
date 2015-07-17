143fa6d0705fe3cdbe8de9f0e6a781af238604b3
fjcapelo <fjcapelo@gmail.com>
2015-07-17 09:13:14 +0200

    Autoload

--- models/models.js
+++ models/models.js
@@ -46,7 +46,7 @@ sequelize.sync().success (function() {
                                 });
                                 Quiz.create ({ pregunta: 'Capital de Francia',
                                                respuesta: 'Paris'
-                                             });
+                                             })
                    .success (function(){console.log('Base de datos inicializada')});
       };
     });
