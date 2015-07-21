650fda6e7a4432ace766cece60975091b5d8db15
fjcapelo <fjcapelo@gmail.com>
2015-07-21 06:49:21 +0200

    Autoload

--- models/models.js
+++ models/models.js
@@ -38,16 +38,12 @@ sequelize.sync().success (function() {
   // success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().success (function (count){
     if(count === 0){ //la tabla se inicializa solo si esta vacia
-      Quiz.create ({ pregunta: 'Capital de Italia',
-                     respuesta: 'Roma'
-                   });
-                   Quiz.create ({ pregunta: 'Capital de Portugal',
-                                  respuesta: 'Lisboa'
-                                });
-                                Quiz.create ({ pregunta: 'Capital de Francia',
-                                               respuesta: 'Paris'
-                                             })
-                   .success (function(){console.log('Base de datos inicializada')});
+      Quiz.bulkCreate(
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'},
+          {pregunta: 'Capital de Francia', respuesta: 'Paris'},
+        ]
+      ).success (function(){console.log('Base de datos inicializada')});
       };
     });
 
