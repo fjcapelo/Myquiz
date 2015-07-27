3c56da1f3cc786fa64203982fd9f66201ca55de7
fjcapelo <fjcapelo@gmail.com>
2015-07-27 07:57:05 +0200

    P2Ptema7

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ res.render('index', { title: 'Quiz' });
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 //definicion de rutas de /quizes
-router.get('/quizes/index', quizController.index);
+router.get('/quizes/index.ejs', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 //Creditos
