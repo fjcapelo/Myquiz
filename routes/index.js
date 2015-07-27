f98b62d1414cb2e72d68059d9b11499a4be2d17e
fjcapelo <fjcapelo@gmail.com>
2015-07-27 07:54:49 +0200

    P2Ptema7

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
 var quizController = require('../controllers/quiz_controller');
-
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+res.render('index', { title: 'Quiz' });
 });
-
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+//definicion de rutas de /quizes
+router.get('/quizes/index', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 //Creditos
 router.get('/author/creditos', function(req, res) {
-  res.render ('author/creditos', {});
+res.render ('author/creditos', {});
 });
-
 module.exports = router;
