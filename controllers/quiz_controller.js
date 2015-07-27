6a2e8d7fc531adccce373e7471c0416038d53f1f
fjcapelo <fjcapelo@gmail.com>
2015-07-27 07:20:57 +0200

    P2Ptema7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,41 @@
-var models = require('../models/models.js');
-// GET /quizes/question
-exports.question = function (req, res) {
-	models.Quiz.findAll().success(function(quiz) {
-	res.render('quizes/question', {pregunta: quiz[0].pregunta});
+var models=require('../models/models.js');
+exports.load=function(req,res,next,quizId){
+models.Quiz.find(quizId).then(
+function(quiz){
+if(quiz){
+req.quiz=quiz;
+next();
+}else{
+next(new Error("No existe quizID=" + quizId));
+}
+}
+).catch(function(error){
+next(error);
+});
+};
+exports.index=function(req,res){
+filtro={};
+if(typeof req.query.search!=="undefined"){
+busca="%"+req.query.search.replace(/\s/gi,'%')+"%";
+filtro={where: ["pregunta like ?",busca]};
+}
+models.Quiz.findAll(filtro).then(function(quizes){
+res.render('quizes/index.ejs',{quizes:quizes});
 })
 };
-
-//GET /quizes/answer
-exports.answer = function (req, res) {
-	models.Quiz.findAll().success(function(quiz){
-	if (req.query.respuesta === quiz[0].respuesta){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer',{respuesta: 'Incorrecto'});
+exports.show=function(req,res){
+res.render('quizes/show',{quiz: req.quiz});
+};
+exports.answer=function(req,res){
+var resultado='Incorrecto';
+if(req.query.respuesta===req.quiz.respuesta){
+resultado='Correcto';
 }
-})
+res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+};
+exports.search=function(req,res){
+var quiz=models.Quiz.build(
+{pregunta: "pregunta",respuesta: "respuesta"}
+);
+res.render('quizes/new.ejs',{quiz: quiz});
 };
